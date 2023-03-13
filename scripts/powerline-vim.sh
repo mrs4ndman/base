008969bcadcca448c97501d747311af1754d33df
MrSandman <121260905+mrs4ndman@users.noreply.github.com>
2023-03-13 18:23:36 +0100

    Upload powerline script

---
+++ scripts/powerline-vim.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+sudo add-apt-repository universe
+sudo apt install -y powerline fonts-powerline python3 python3-pip python3-setuptools
+
+echo "
+if [ -f /usr/share/powerline/bindings/bash/powerline.sh ]; then
+	powerline-daemon -q
+	POWERLINE_BASH_CONTINUATION=1
+	POWERLINE_BASH_SELECT=1
+	source /usr/share/powerline/bindings/bash/powerline.sh
+fi">> $HOME/.bashrc
+
+echo "
+python3 from powerline.vim import setup as powerline_setup
+python3 powerline_setup()
+python3 del powerline_setup
+
+set laststatus=2">> $HOME/.vimrc
+
+mkdir -p $HOME/.config/powerline
+cp -R /usr/share/pwoerline/config_files/* \
+	$HOME/.config/powerline/
+
+powerline-daemon --replace
+
+#Añadir "args": { "status_colors": true }
+#Luego hay que ejecutar source $HOME/.bashrc para finalizar los cambios.
+
