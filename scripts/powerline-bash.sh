d65360b6373327ae7af800cc453b5eb4d9823243
MrSandman <121260905+mrs4ndman@users.noreply.github.com>
2023-03-18 01:30:36 +0100

    Cambio de uso con powerline (NO USAR TODAVIA)
    He empezado a usar airlline en vim en vez de powerline, por lo que queda relegado a bash como embellecedor, este edit es para quitar la parte que modificaba el .vimrc

--- scripts/powerline-bash.sh
+++ scripts/powerline-bash.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#Probado con PopOS (y su terminal por defecto), Tilix y VIM 
-sudo add-apt-repository universe
-sudo apt install -y powerline fonts-powerline python3 python3-pip python3-setuptools
+#Probado con PopOS (y su terminal por defecto), Tilix y VIM
+#Técnicamente compatible con Ubuntu / Debian y derivados. Ejecutar con bash o haciendo chmod 750 powerline-vim.sh y llamando al archivo.
+sudo apt install -y powerline fonts-powerline
 
 echo "
 if [ -f /usr/share/powerline/bindings/bash/powerline.sh ]; then
@@ -10,13 +10,6 @@ if [ -f /usr/share/powerline/bindings/bash/powerline.sh ]; then
 	POWERLINE_BASH_SELECT=1
 	source /usr/share/powerline/bindings/bash/powerline.sh
 fi">> $HOME/.bashrc
-
-echo "
-python3 from powerline.vim import setup as powerline_setup
-python3 powerline_setup()
-python3 del powerline_setup
-
-set laststatus=2">> $HOME/.vimrc
 
 mkdir -p $HOME/.config/powerline
 cp -R /usr/share/powerline/config_files/* \
@@ -27,12 +20,16 @@ powerline-daemon --replace
 echo "Añadir \"args\": { \"status_colors\": \true } a la parte 
 de .vcs.branch de .config/powerline/themes/shell/default_leftonly.json"
 #Luego hay que ejecutar source $HOME/.bashrc para finalizar los cambios.
-#Fuente a usar en terminal por defecto: Droid Sans Mono for Powerline
+#Fuente que uso yo en mi terminal: Droid Sans Mono for Powerline
 
 # clonamos el repositorio de fuentes
 git clone https://github.com/powerline/fonts.git --depth=1
 # instalamos
 cd fonts
+./install.sh
+# limpiamos un poquito las cosas
+cd ..
+rm -rf fonts
 ./install.sh
 # limpiamos un poquito las cosas
 cd ..
