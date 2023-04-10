24dcb1cb4273c31a575d851dabc42ecb3838bea6
MrSandman <mrsandman.h4sh@gmail.com>
2023-04-10 15:09:46 +0200

    Removed powerline (moving to Starship)

--- scripts/powerline-bash.sh
+++ scripts/powerline-bash.sh
@@ -1,28 +1,7 @@
 #!/bin/bash
 #Probado con PopOS (y su terminal por defecto), Tilix y VIM
-#Técnicamente compatible con Ubuntu / Debian y derivados. Ejecutar con bash o haciendo chmod 750 powerline-vim.sh y llamando al archivo.
-sudo apt install -y powerline fonts-powerline
+#Técnicamente compatible con Ubuntu / Debian y derivados. 
 
-echo "
-if [ -f /usr/share/powerline/bindings/bash/powerline.sh ]; then
-	powerline-daemon -q
-	POWERLINE_BASH_CONTINUATION=1
-	POWERLINE_BASH_SELECT=1
-	source /usr/share/powerline/bindings/bash/powerline.sh
-fi">> $HOME/.bashrc
-
-mkdir -p $HOME/.config/powerline
-cp -R /usr/share/powerline/config_files/* \
-	$HOME/.config/powerline/
-
-powerline-daemon --replace
-
-echo "Añadir \"args\": { \"status_colors\": \true } a la parte 
-de .vcs.branch de .config/powerline/themes/shell/default_leftonly.json"
-#Luego hay que ejecutar source $HOME/.bashrc para finalizar los cambios.
 #Fuente que uso yo en mi terminal: Droid Sans Mono de Nerd Font o Powerline.
 
-# clonamos el repositorio de fuentes
-wget https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf && 
-sudo cp Droid\ Sans\ Mono\ Nerd\ Font\ Complete.otf /usr/share/fonts/
 
